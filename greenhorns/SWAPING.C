bbecf3d830a46bb95c211127934edf89b7c79ab2
AKSHIT SHELIYA <121547171+akshitsheliya@users.noreply.github.com>
2022-12-31 08:41:31 +0530

    Add files via upload

---
+++ greenhorns/SWAPING.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+main()
+    {
+
+       int a,b,c;
+     clrscr  ();
+
+    printf("enter the value of a :-",a);
+    scanf("%d",&a);
+
+     printf("enter the value of b :-",b);
+    scanf("%d",&b);
+
+    c=a ;
+    a=b;
+    b=c ;
+printf("addtion is :- %d\n",a);
+printf("addtion is :- %d\n",a);
+
+    getch();
+
+}
---
+++ greenhorns/cel.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+main()
+    {
+
+       int c,f;
+     clrscr  ();
+
+    printf("enter the value of a :-");
+    scanf("%d",&c);
+
+
+     f= 1.8*c+32    ;
+	   printf("celcius to faranhit is :- %d\n",f);
+
+    getch();
+
+}
